5bc9abd5810f43cccc008af38128ce5e287a2224
Marden Rafael <mardenrafaeldalmagro.gimenez@gmail.com>
2023-04-21 04:00:51 -0400

    feat: cria RouterManager

---
+++ src/manager/RouterManager.ts
@@ -0,0 +1,31 @@
+import Application from "../Application";
+import { AbstractRouter } from "../routes/abstractRouter";
+import ApplicationManager from "./ApplicationManager";
+
+export default class RouterManager {
+  private static instance: RouterManager;
+  private readonly app: Application;
+  private readonly routes: AbstractRouter[] = [];
+
+  private constructor() {
+    this.app = ApplicationManager.getInstance();
+  }
+
+  public static getInstance(): RouterManager {
+    if (!this.instance) {
+      this.instance = new RouterManager();
+    }
+
+    return this.instance;
+  }
+
+  public add<T extends AbstractRouter>(router: T) {
+    this.routes.push(router);
+  }
+
+  public mountRoutes(): void {
+    this.routes.forEach(route => {
+      this.app.mountRoute(route.export());
+    });
+  }
+}
