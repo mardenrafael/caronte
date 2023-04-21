5dfdad501221f017d656270d8e392ecdcd39be37
Marden Rafael <mardenrafaeldalmagro.gimenez@gmail.com>
2023-04-21 16:32:43 -0400

    feat: transforma applicationManager em um singleton

--- src/manager/ApplicationManager.ts
+++ src/manager/ApplicationManager.ts
@@ -1,13 +1,37 @@
 import Application from "../Application";
+import Logger from "../utils/Logger";
 
 export default class ApplicationManager {
-  private static instance: Application;
+  private static instance: ApplicationManager;
+  private application: Application | undefined;
+  private readonly logger: Logger;
+  private isInitialize: boolean = false;
 
-  public static getInstance(): Application {
+  private constructor() {
+    this.logger = new Logger();
+  }
+
+  public static getApplicationInstance(): ApplicationManager {
     if (!this.instance) {
-      this.instance = new Application();
+      this.instance = new ApplicationManager();
     }
 
     return this.instance;
+  }
+
+  public initializeApplication(): void {
+    if (this.isInitialize) {
+      this.logger.log("Application already initialize");
+      return;
+    }
+    this.application = new Application();
+    this.isInitialize = true;
+  }
+
+  public getApplication(): Application {
+    if (!this.isInitialize) {
+      this.initializeApplication();
+    }
+    return this.application!;
   }
 }
