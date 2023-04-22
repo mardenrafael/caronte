6ede10eb92a81279a9fd4565881b9b48cfa26039
Marden Rafael <mardenrafaeldalmagro.gimenez@gmail.com>
2023-04-22 01:04:52 -0400

    feat: cria abstract controller

--- src/controller/AbstractController/AbstractController.ts
+++ src/controller/AbstractController/AbstractController.ts
@@ -1,13 +1,41 @@
-import { Request, RequestHandler, Response } from "express";
+import { RequestHandler } from "express";
 import { HttpMethods } from "../../router/abstractRouter/AbstractRouter";
 
 export type RequestHandlerDescriptor = {
-  handlerKey: PropertyKey;
   handler: RequestHandler;
   method: HttpMethods;
   parameters?: String;
 };
 
 export default abstract class AbstractController {
-  public abstract handler(req: Request, res: Response): Promise<void>;
+  private readonly definedHandlers: RequestHandlerDescriptor[];
+
+  constructor() {
+    this.definedHandlers = [];
+  }
+
+  public getHandler(method: HttpMethods): RequestHandler {
+    let foundHandler;
+
+    for (const handler of this.definedHandlers) {
+      if (handler.method == method) {
+        foundHandler = handler;
+        break;
+      }
+    }
+
+    if (!foundHandler) {
+      throw new Error("Endpoint not available or does not exist");
+    }
+
+    return foundHandler.handler;
+  }
+
+  protected addHandler(
+    requestHandlerDescriptor: RequestHandlerDescriptor,
+  ): void {
+    this.definedHandlers.push(requestHandlerDescriptor);
+  }
+
+  public abstract setupHandlers(): void;
 }
