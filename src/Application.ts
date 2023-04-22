45872f2a5a3f907df13613790b78a7d85d042d7c
Marden Rafael <mardenrafaeldalmagro.gimenez@gmail.com>
2023-04-22 19:16:32 -0400

    refac: move logica de configuração para o Manager correto

--- src/Application.ts
+++ src/Application.ts
@@ -1,61 +1,12 @@
-import express, { Express, Router, json } from "express";
-import morgan from "morgan";
+import express, { Express, Router } from "express";
 
 export default class Application {
   private readonly app: Express = express();
+  private dev: boolean = true;
   private port: number | undefined;
   private host: string | undefined;
-  private dev: boolean = true;
-
-  private isConfigDone: boolean = false;
-
-  constructor() {
-    const nodeEnv = process.env["NODE_ENV"];
-    const portStr = process.env["PORT"];
-    const host = process.env["HOST"];
-
-    if (!portStr) {
-      throw new Error("Error on load env vars");
-    }
-
-    if (!host) {
-      throw new Error("Error on load env vars");
-    }
-
-    if (!nodeEnv) {
-      throw new Error("Error on load env vars");
-    }
-
-    this.port = Number.parseInt(portStr);
-    this.host = host;
-
-    if (nodeEnv === "development") {
-      this.dev = true;
-    } else {
-      this.dev = false;
-    }
-  }
-
-  public setPort(port: number): void {
-    this.port = port;
-
-    if (this.dev) {
-      console.log(`Port load: ${port}`);
-    }
-  }
-
-  public setHost(host: string): void {
-    this.host = host;
-    if (this.dev) {
-      console.log(`Host load: ${host}`);
-    }
-  }
 
   public use(handler: any): void {
-    if (!this.isConfigDone) {
-      this.config();
-    }
-
     this.app.use(handler);
     if (this.dev) {
       console.log(`New handler register`);
@@ -63,34 +14,23 @@ export default class Application {
   }
 
   public mountRoute(route: Router): void {
-    if (!this.isConfigDone) {
-      this.config();
-    }
     this.app.use(route);
   }
 
-  public config(): void {
-    this.app.use(json());
-    this.app.use(morgan("dev"));
+  public setPort(port: number): void {
+    this.port = port;
+  }
 
-    this.isConfigDone = true;
-
-    if (this.dev) {
-      console.log("Config setup done!");
-    }
+  public setHost(host: string): void {
+    this.host = host;
   }
 
   public start(): void {
     if (!this.port) {
-      throw new Error("'port' not defined properly");
+      throw new Error("Port is not defined properly");
     }
-
     if (!this.host) {
-      throw new Error("'host' not defined properly");
-    }
-
-    if (!this.isConfigDone) {
-      this.config();
+      throw new Error("Host is not defined properly");
     }
 
     this.app.listen(this.port, this.host, () => {
--- src/manager/ApplicationManager.ts
+++ src/manager/ApplicationManager.ts
@@ -1,14 +1,29 @@
+import { json } from "express";
 import Application from "../Application";
 import Logger from "../utils/Logger";
+import morgan from "morgan";
+import ControllerManager from "./ControllerManager";
+import RouterManager from "./RouterManager";
+import { AbstractRouter } from "../router/abstractRouter";
+import { AbstractController } from "../controller/AbstractController";
 
 export default class ApplicationManager {
   private static instance: ApplicationManager;
   private application: Application | undefined;
   private readonly logger: Logger;
+  private port!: number;
+  private host!: string;
   private isInitialize: boolean = false;
+  private isDevMode: Boolean = false;
+  private isConfig: Boolean = false;
+  private readonly controllerManager: ControllerManager;
+  private readonly routerManager: RouterManager;
 
   private constructor() {
     this.logger = new Logger();
+    this.controllerManager =
+      ControllerManager.getControlllerManagerInstance();
+    this.routerManager = RouterManager.getRouterManagerInstance();
   }
 
   public static getApplicationManagerInstance(): ApplicationManager {
@@ -24,7 +39,9 @@ export default class ApplicationManager {
       this.logger.log("Application already initialize");
       return;
     }
+
     this.application = new Application();
+    this.config();
     this.isInitialize = true;
   }
 
@@ -32,6 +49,87 @@ export default class ApplicationManager {
     if (!this.isInitialize) {
       this.initializeApplication();
     }
+
+    this.config();
     return this.application!;
+  }
+
+  private config(): void {
+    if (!this.application) {
+      throw new Error("Application is not load properly");
+    }
+
+    if (this.isConfig) {
+      return;
+    }
+
+    const nodeEnv = process.env["NODE_ENV"];
+    const portStr = process.env["PORT"];
+    const host = process.env["HOST"];
+
+    if (!portStr) {
+      throw new Error("Error on load env vars");
+    }
+
+    if (!host) {
+      throw new Error("Error on load env vars");
+    }
+
+    if (!nodeEnv) {
+      throw new Error("Error on load env vars");
+    }
+
+    this.port = Number.parseInt(portStr);
+    this.host = host;
+
+    if (nodeEnv === "development") {
+      this.isDevMode = true;
+    } else {
+      this.isDevMode = false;
+    }
+
+    this.application.use(json());
+    this.application.use(morgan("dev"));
+
+    this.application.setPort(this.port);
+    this.application.setHost(this.host);
+
+    this.isConfig = true;
+    if (this.isDevMode) {
+      console.log("Config setup done!");
+    }
+  }
+
+  public addRouter(router: AbstractRouter): void {
+    this.routerManager.add(router);
+  }
+
+  public addController(controller: AbstractController): void {
+    this.controllerManager.add(controller);
+  }
+
+  public add<T extends AbstractController | AbstractRouter>(
+    item: T,
+  ): void {
+    if (item instanceof AbstractController) {
+      this.controllerManager.add(item);
+      return;
+    }
+
+    if (item instanceof AbstractRouter) {
+      this.routerManager.add(item);
+    }
+  }
+
+  public start(): void {
+    if (!this.application) {
+      throw new Error("Application is not load properly");
+    }
+
+    this.controllerManager.setupAllHandlers();
+    this.routerManager.setupRoute();
+    this.routerManager.mountRoutes(this.application);
+
+    this.application.start();
   }
 }
