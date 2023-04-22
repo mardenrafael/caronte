70a8a3fff3f6772ee00b62a300e97e6ca5e4ec5f
Marden Rafael <mardenrafaeldalmagro.gimenez@gmail.com>
2023-04-22 01:05:55 -0400

    feat: cria user router e user controller

--- src/controller/UserController/UserController.ts
+++ src/controller/UserController/UserController.ts
@@ -1,13 +1,31 @@
 import { Request, Response } from "express";
+import { HttpMethods } from "../../router/abstractRouter/AbstractRouter";
 import { AbstractController } from "../AbstractController";
 
 export default class UserController extends AbstractController {
-  public override async handler(
-    _req: Request,
-    res: Response,
-  ): Promise<void> {
+  constructor() {
+    super();
+  }
+
+  private async teste01(_req: Request, res: Response): Promise<void> {
     res.json({
-      message: "sucess",
+      message: "ok",
+    });
+  }
+  private async teste02(_req: Request, res: Response): Promise<void> {
+    res.json({
+      message: "ok",
+    });
+  }
+
+  public override setupHandlers(): void {
+    super.addHandler({
+      handler: this.teste01,
+      method: HttpMethods.GET,
+    });
+    super.addHandler({
+      handler: this.teste02,
+      method: HttpMethods.POST,
     });
   }
 }
