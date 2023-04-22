6e8b9bf5e16662092a8d3937b48a646eeef99780
Marden Rafael <mardenrafaeldalmagro.gimenez@gmail.com>
2023-04-21 23:43:12 -0400

    fix: corrige erro de grafia

--- src/utils/Logger.ts
+++ src/utils/Logger.ts
@@ -1,4 +1,4 @@
-// Avalira ideia de futaramento virar um framework | com suporte ao express como middleware e um logger completo
+// Avaliar ideia de no futuro virar um framework | com suporte ao express como middleware e um logger completo
 export default class Logger {
   public log(message: string): void {
     console.log(message);
