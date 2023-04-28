21799f4683b61a02f5dc2d4dffd3fcd73212c644
Marden Rafael <mardenrafaeldalmagro.gimenez@gmail.com>
2023-04-27 21:39:09 -0400

    test: cria testes

--- test/manager/ApplicationManager.spec.ts
+++ test/manager/ApplicationManager.spec.ts
@@ -1,11 +1,12 @@
 import Application from "../../src/Application";
 import ApplicationManager from "../../src/manager/ApplicationManager";
+import Manager from "../../src/manager/Manager";
 
 describe("ApplicationManager: ", () => {
   it("Deve retornar uma instance de ApplicationManager", () => {
     const manager = ApplicationManager.getApplicationManagerInstance();
 
-    expect(manager).toBeInstanceOf(ApplicationManager);
+    expect(manager).toBeInstanceOf(Manager);
   });
 
   it("Deve retornar uma instancia da aplicação", () => {
@@ -13,5 +14,30 @@ describe("ApplicationManager: ", () => {
     const application = manager.getApplicationInstance();
 
     expect(application).toBeInstanceOf(Application);
+  });
+
+  it("Deve conter atributo port e host como undefined quando isConfig é false", () => {
+    const manager = ApplicationManager.getApplicationManagerInstance();
+
+    expect(manager.isConfig()).toBeFalsy();
+    expect(manager.getHost()).toBeUndefined();
+    expect(manager.getPort()).toBeUndefined();
+  });
+
+  it("Deve conter atributo port e host quando isConfig é true", () => {
+    const manager = ApplicationManager.getApplicationManagerInstance();
+
+    manager.config();
+
+    expect(manager.isConfig()).toBeTruthy();
+    expect(manager.getHost()).toBeDefined();
+    expect(manager.getPort()).toBeDefined();
+  });
+
+  it("Deve conter controller manager", () => {
+    const manager = ApplicationManager.getApplicationManagerInstance();
+
+    expect(manager.getControllerManager()).toBeDefined();
+    expect(manager.getControllerManager()).toBeInstanceOf(Manager);
   });
 });
