56bc49ca741d637912f88cf5a4b740e0e0c3f2f5
Marden Rafael <mardenrafaeldalmagro.gimenez@gmail.com>
2023-04-30 01:50:26 -0400

    feat: adiciona healthCheck

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -2,6 +2,8 @@ import * as dotenv from "dotenv";
 import { UserController } from "../controller/UserController";
 import ApplicationManager from "../manager/ApplicationManager";
 import UserRouter from "../router/userRouter/UserRouter";
+import HealthCheckController from "../api/healthCheck/HealthCheckController";
+import HealthCheckRouter from "../api/healthCheck/HealthCheckRouter";
 
 dotenv.config();
 
@@ -14,8 +16,13 @@ const userRouter = new UserRouter({
   controller: userController,
 });
 
+const healthCheckController = new HealthCheckController();
+const healthCheckRouter = new HealthCheckRouter(healthCheckController);
+
 applicationManager.add(userRouter);
 applicationManager.add(userController);
+applicationManager.add(healthCheckRouter);
+applicationManager.add(healthCheckController);
 applicationManager.initApplication();
 applicationManager.config();
 applicationManager.setup();
