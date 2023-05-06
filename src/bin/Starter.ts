8e656b6799b9327e7fe695e14a1077eba464dab6
Marden Rafael <mardenrafaeldalmagro.gimenez@gmail.com>
2023-05-05 20:36:27 -0400

    refac: inicia healthCheck no starter

--- src/bin/Starter.ts
+++ src/bin/Starter.ts
@@ -1,6 +1,10 @@
 import Application from "./Application";
 import ApplicationManager from "../manager/ApplicationManager";
 import Manager from "../manager/Manager";
+import {
+  HealthCheckController,
+  HealthCheckRouter,
+} from "../api/healthCheck";
 
 export default class Starter {
   private readonly applicationManager: Manager<Application>;
@@ -10,8 +14,24 @@ export default class Starter {
       ApplicationManager.getApplicationManagerInstance();
   }
 
+  private setupHealthChecker(): void {
+    if (this.applicationManager instanceof ApplicationManager) {
+      const healthCheckController = new HealthCheckController();
+      const healthCheckRouter = new HealthCheckRouter(
+        healthCheckController,
+      );
+
+      this.applicationManager.add(healthCheckController);
+      this.applicationManager.add(healthCheckRouter);
+    } else {
+      throw new Error("Error on setup healthchecker");
+    }
+  }
+
   public start(): void {
     if (this.applicationManager instanceof ApplicationManager) {
+      this.setupHealthChecker();
+
       this.applicationManager.initApplication();
       this.applicationManager.config();
       this.applicationManager.setup();
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,8 @@
 import * as dotenv from "dotenv";
+import { Starter } from "./bin";
 import { UserController } from "./controller/UserController";
 import { ApplicationManager } from "./manager";
 import { UserRouter } from "./router/userRouter";
-import {
-  HealthCheckRouter,
-  HealthCheckController,
-} from "./api/healthCheck";
-import { Starter } from "./bin";
 
 dotenv.config();
 
@@ -18,12 +14,7 @@ const applicationManager =
 const userController = new UserController();
 const userRouter = new UserRouter(userController);
 
-const healthCheckController = new HealthCheckController();
-const healthCheckRouter = new HealthCheckRouter(healthCheckController);
-
 applicationManager.add(userRouter);
 applicationManager.add(userController);
-applicationManager.add(healthCheckRouter);
-applicationManager.add(healthCheckController);
 
 starter.start();
