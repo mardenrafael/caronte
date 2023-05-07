5131ad76a3255af0818ea13f973adbbe523f6435
Marden Rafael <mardenrafaeldalmagro.gimenez@gmail.com>
2023-05-07 01:07:16 -0400

    refac: adiciona tipagem as variaveis

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,20 @@
 import * as dotenv from "dotenv";
 import { Starter } from "./bin";
+import { Controller } from "./controller/Controller";
 import { UserController } from "./controller/UserController";
 import { ApplicationManager } from "./manager";
+import { Router } from "./router/Router";
 import { UserRouter } from "./router/userRouter";
 
 dotenv.config();
 
 const starter = new Starter();
 
-const applicationManager =
+const applicationManager: ApplicationManager =
   ApplicationManager.getApplicationManagerInstance();
 
-const userController = new UserController();
-const userRouter = new UserRouter(userController);
+const userController: Controller = new UserController();
+const userRouter: Router = new UserRouter(userController);
 
 applicationManager.add(userRouter);
 applicationManager.add(userController);
